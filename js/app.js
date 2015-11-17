222f0e68d16ea1f28c86c1189bc1c94607f01cb5
grantb4812 <grantb4812@gmail.com>
2015-11-17 22:28:21 +0000

    working on bio and nav routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+app = angular.module('larryByrd', ['ngRoute']);
 
+app.config(function($routeProvider){
+   $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html'
+        })
+        
+        .when('/bio', {
+            templateUrl: 'views/bio.html'
+        })
+});
+
+app.controller('navigationController', function($scope){
+    $scope.message = "This is working;"
+});
