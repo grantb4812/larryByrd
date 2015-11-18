c099cdfb07615cad91bcb693e8cc5eb59899dd45
grantb4812 <grantb4812@gmail.com>
2015-11-18 04:03:18 +0000

    fixed bio centering

--- js/app.js
+++ js/app.js
@@ -2,15 +2,25 @@ app = angular.module('larryByrd', ['ngRoute']);
 
 app.config(function($routeProvider){
    $routeProvider
-        .when('/', {
+        .when('/home', {
+            templateUrl: 'views/home.html'
+        })
+        
+        .when('/home', {
             templateUrl: 'views/home.html'
         })
         
         .when('/bio', {
             templateUrl: 'views/bio.html'
         })
+        
+        .when('/music', {
+            templateUrl: 'views/music.html',
+            controller: 'musicController'
+            
+        })
 });
 
-app.controller('navigationController', function($scope){
+app.controller('musicController', function($scope){
     $scope.message = "This is working;"
 });
